dd353b3ce9e9f896bb9aab04f935238a73f4cd0a
unknown <ahninuraini.srg@gmail.com>
2020-12-18 18:46:51 +0700

    tyastin0654

--- app/src/main/java/com/appcosmetics/app/data/model/ActionState.kt
+++
@@ -1,8 +0,0 @@
-package com.appcosmetics.app.data.model
-
-data class ActionState<T>(
-    val data: T? = null,
-    val message: String? = null,
-    val isSuccess: Boolean = true,
-    var isConsumed: Boolean = false,
-)
